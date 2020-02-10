8318a07d5df681e73d31ca7bb1fee90d80a0a23d
Yize <yize.zhao@hotmail.com>
2020-02-09 20:01:17 -0500

    init implementation of part 3

--- lab3/Task3.c
+++ lab3/Task3.c
@@ -2,7 +2,7 @@ code Main
 
   -- OS Class: Project 3
   --
-  -- <PUT YOUR NAME HERE>
+  -- <Yize Zhao>
   --
 
   -- This package contains the following:
@@ -19,16 +19,39 @@ code Main
   var
     GP: GamingParlor = new GamingParlor
     -- Hint: Some variables are defined in "Task3.h".
+    game_groups: array[total_groups] of Thread
+    enough_dice: Condition
+    mutexlock: Mutex
+
 
   function gamingparlor()
     GP.Init()
+    game_groups = new array of Thread{total_groups of new Thread}
+    mutexlock = new Mutex
+    mutexlock.Init()
+
+    enough_dice = new Condition
+    enough_dice.Init()
+
 
     -- Remove the following line in your implementation
     -- This is only an example.
     GP.PrintExample()
 
     -- Add more code below
+    i = 0
+    for (i=0;i<total_groups;i=i+1)
+        game_groups[i].Init(group_names[i])
+        game_groups[i].Fork(game, i)
+    endFor
   
+  endFunction
+
+  function game(group_idx: int)
+    GP.Request(group_names[group_idx], dice_per_group[group_idx])
+    currentThread.Yield()
+    GP.Return(group_names[group_idx], dice_per_group[group_idx])
+
   endFunction
 
   behavior GamingParlor
@@ -38,17 +61,23 @@ code Main
     endMethod
 
     method Request(name: char, nr_of_dice: int)
+      mutexlock.Lock()
       self.Print(name, "requests", nr_of_dice)
       while dice_available < nr_of_dice
-        FatalError ("Needs to be implemented")
+        -- FatalError ("Needs to be implemented")
+        enough_dice.wait(&mutexlock)
       endWhile
       dice_available = dice_available - nr_of_dice
       self.Print(name, "proceeds with", nr_of_dice)
+        mutexlock.Unlock()
     endMethod
 
     method Return(name: char, nr_of_dice: int)
+        mutexlock.Lock()
       dice_available = dice_available + nr_of_dice
       self.Print(name, "releases and adds back", nr_of_dice)
+        enough_dice.Broadcast(&mutexlock)
+        mutexlock.Unlock()
     endMethod
 
     method PrintExample()
